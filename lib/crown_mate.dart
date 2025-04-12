564d65c683cd5a320a9cf81a8c9b3ada0d954bc8
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-04-12 20:12:52 +0200

    Changes to be committed: 	modified:   lib/crown_mate.dart 	renamed:    lib/feathers/pages/home_page/ui/page/home_page.dart -> lib/features/pages/home_page/ui/page/home_page.dart

--- lib/crown_mate.dart
+++ lib/crown_mate.dart
@@ -1,5 +1,4 @@
-
-import 'package:crown_mate/feathers/pages/home_page/ui/page/home_page.dart';
+import 'package:crown_mate/features/pages/home_page/ui/page/home_page.dart';
 import 'package:flutter/material.dart';
 
 class CrownMate extends StatelessWidget {
@@ -9,9 +8,27 @@ class CrownMate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Crown Mate',
+
+      //theme: ,
+      //darkTheme: ,
+      //navigatorKey: ,
+      //initialRoute: ,
+      // onGenerateRoute: appRouter.generateRoute,
+      // themeAnimationCurve: Curves.easeInOut,
+      // themeAnimationDuration: Durations.short1,
+      // locale: const Locale('ar', 'EG'),
+      // supportedLocales: const [Locale('ar', 'EG')],
+      // localizationsDelegates: const [
+      //   S.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // localeResolutionCallback:
+      //     (locale, supportedLocales) => const Locale('ar', 'EG'),
       home: HomePage(),
     );
   }
 }
-
